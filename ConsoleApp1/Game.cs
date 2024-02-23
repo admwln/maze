8041a826d255203e1606ab93450bed2a64523ae7
Adam Welin <welinadam@users.noreply.github.com>
2024-02-23 15:19:53 +0100

    Add logic for two players

--- ConsoleApp1/Game.cs
+++ ConsoleApp1/Game.cs
@@ -4,11 +4,47 @@ class Game
     ScheduleTimer? _timer;
     public bool Paused { get; private set; }
     public bool GameOver { get; private set; }
-    public void Start()
+    public void Start(Game game, Maze maze)
     {
         //Console.WriteLine("Start");
         
         ScheduleNextTick();
+        while (!game.GameOver)
+        {
+            // Listen to keys
+            //var ch = Console.ReadKey(false).Key;
+            var keyInfo = Console.ReadKey(true);
+            
+            switch (keyInfo.Key)
+            {
+                // PlayerOne
+                case ConsoleKey.UpArrow:
+                    maze.Move("up", maze.PlayerOne);
+                    break;
+                case ConsoleKey.DownArrow:
+                    maze.Move("down", maze.PlayerOne);
+                    break;
+                case ConsoleKey.RightArrow:
+                    maze.Move("right", maze.PlayerOne);
+                    break;
+                case ConsoleKey.LeftArrow:
+                    maze.Move("left", maze.PlayerOne);
+                    break;
+                // PlayerTwo
+                case ConsoleKey.W:
+                    maze.Move("W", maze.PlayerTwo);
+                    break;
+                case ConsoleKey.S:
+                    maze.Move("S", maze.PlayerTwo);
+                    break;
+                case ConsoleKey.A:
+                    maze.Move("A", maze.PlayerTwo);
+                    break;
+                case ConsoleKey.D:
+                    maze.Move("D", maze.PlayerTwo);
+                    break;
+            }
+        }
     }
 
     public void Pause()
--- ConsoleApp1/Player.cs
+++ ConsoleApp1/Player.cs
@@ -1,9 +1,14 @@
+using System.Drawing;
+
 namespace ConsoleApp1;
 
-public class Player(string name)
+public class Player(string name, ConsoleColor color, int gridValue, int scoreCursorPosition)
 {
     public string Name { get; } = name;
     public int Score { get; set;  } = 0;
+    public int ScoreCursorPosition { get; set;  } = scoreCursorPosition;
+    public ConsoleColor Color { get; set; } = color;
+    public int GridValue { get; set; } = gridValue;
 
     public void AddToScore(int value)
     {
